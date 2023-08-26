b0bf8f635c0d7ca1c05af3974ee520abf04338c9
yousiki <You.Siki@outlook.com>
2023-08-26 14:39:57 +0800

    fix bug

--- addon/CircularLight_Op.py
+++ addon/CircularLight_Op.py
@@ -7,12 +7,12 @@ from .utils import *
 from .Output_Op import get_all
 from .Light_Op import create_light, set_light_direction
 
-def circular_light(context, num_light=3):
+def circular_light(context, num_light=3, phi=45):
     delete_all(context, ['LIGHT'])
     ls = []
     for i in range(num_light):
         light = create_light(context)
-        ld = set_light_direction(light, 360 / num_light * i, 45)
+        ld = set_light_direction(light, 360 / num_light * i, phi)
         li = light.data.energy
         lc = light.data.color
         ls.append({
@@ -27,5 +27,5 @@ class RENDER_OT_CIRCULAR_LIGHT(Operator):
     bl_label = 'Set Circular Lights'
     
     def execute(self, context):
-        circular_light(context, num_light=context.scene.num_circular_lights)
+        circular_light(context, num_light=context.scene.num_circular_lights, phi=context.scene.circular_phi)
         return {'FINISHED'}
